409a244697a9be813c676b3bfb6a6034a32efd0d
thijsfranck <thijsfranck@gmail.com>
2025-08-12 16:10:47 +0000

    fix: improve placeholder aspect ratio

--- app/calm_calatheas/components/camera.py
+++ app/calm_calatheas/components/camera.py
@@ -15,7 +15,9 @@ TEMPLATE = """
 <div class="modal is-active">
     <div class="modal-background"></div>
     <div class="modal-content">
-        <video id="camera-stream" width="100%" autoplay playsinline></video>
+        <figure id="camera-container" class="image is-4by3">
+            <video id="camera-stream" width="100%" autoplay playsinline></video>
+        </figure>
         <div class="buttons has-addons is-centered is-large mt-5">
             <button id="camera-capture" class="button is-success is-large is-expanded">Capture</button>
             <button id="camera-switch" class="button is-large">
@@ -50,6 +52,7 @@ class Camera(Component):
     @override
     def on_render(self) -> None:
         self._camera_capture = document.getElementById("camera-capture")
+        self._camera_container = document.getElementById("camera-container")
         self._camera_close = document.getElementById("camera-close")
         self._camera_stream = cast("JsVideoElement", document.getElementById("camera-stream"))
         self._camera_switch = document.getElementById("camera-switch")
@@ -98,11 +101,11 @@ class Camera(Component):
         if not stream:
             self._camera_capture.setAttribute("disabled", "")
             self._camera_switch.setAttribute("disabled", "")
-            self._camera_stream.classList.add("is-skeleton")
+            self._camera_container.classList.add("is-skeleton")
         else:
             self._camera_capture.removeAttribute("disabled")
             self._camera_switch.removeAttribute("disabled")
-            self._camera_stream.classList.remove("is-skeleton")
+            self._camera_container.classList.remove("is-skeleton")
 
     def _handle_post_capture(self, photo: Blob) -> None:
         """Download the captured photo."""
